5ca06eeb8b6f57b572df24f175a94b102a837b97
unknown <jonny@iaansec.com>
2023-12-17 14:25:03 -0500

    assets & README added

--- pybuilder.py
+++ pybuilder.py
@@ -37,6 +37,7 @@ from tkinter.filedialog import askopenfilename
 
 
 logo='''
+\n\n
   _____       _           _ _     _           
  |  __ \     | |         (_) |   | |          
  | |__) |   _| |__  _   _ _| | __| | ___ _ __ 
