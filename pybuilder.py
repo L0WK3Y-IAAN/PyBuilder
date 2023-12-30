c3dfd67daa5471005db64c89f45179d55e85b00a
unknown <jonny@iaansec.com>
2023-12-29 20:59:24 -0500

    Fixed Multi-File Upload, Input Sanitization for directories and files

--- pybuilder.py
+++ pybuilder.py
@@ -1,19 +1,27 @@
+# encoding: utf-8
+
+
+# WORKING BUT DOES NOT HANDLE FILES WITH PARENTHESE CHARACTERS AND SPACES
+
+
 # 🔵 Create a web server that automatically converts the python files that a user uploads and sends it back to them.
 # 🔴 Add Mac OS X alternative commands
 # 🔴 Add Windows version of script
 # 🔴 Pull programmer quotes from the web for loading animation (requires requests module)
+# 🟢 Add multi-file upload
 # 🟢 Add input sanitization to file_selection and output strings
 # 🟢 Fix script so it reads directories and output names with spaces 
 # 🟢 Add package check function that installs needed packages (cython, gcc, tkinter)
 # 🟡 Make pip requirements file for packages that aren't installed (Package check function may need to be altered)
 # 🟡 Add backwards compatibility for older python versions (Might not need if doing a package check)
-
 '''
 🔵 - Top priority
 🟢 - Completed and working as intended
 🟡 - May not need at the moment or feature is subject to change
 🔴 - Not implemented yet
 '''
+
+
 
 
 '''
@@ -46,14 +54,16 @@ https://github.com/L0WK3Y-IAAN
 import os
 import re
 import sys
+import uuid
 import shlex
+import shutil
 import random
 import itertools
 import threading
 import subprocess
 import tkinter as tk
 from time import sleep
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilenames
 
 
 
@@ -78,7 +88,7 @@ done = False
 log_file_path = 'compilation_log.txt'
 
 def sanitize_filename(filename):
-    return re.sub(r'[\\/*?:"<>|]', "", filename)
+    return re.sub(r'[\\/*?:"<>|()!@#$%^&`~ ]', "_", filename)
 
 
 def animate():
@@ -103,6 +113,49 @@ def print_colored_text(text, color_code):
     print(f"\033[{color_code}m{text}\033[0m")
     return
 
+
+
+
+def sanitize_and_rename_file(file_path):
+
+
+
+    # Extract the directory and file name from the file path
+    directory, filename = os.path.split(file_path)
+
+    # If the original filename is empty, generate a unique filename
+    if not filename:
+        filename = f"untitled_{uuid.uuid4().hex}"
+
+    # Remove invalid characters from the file name
+    sanitized_filename = re.sub(r'[\\/*?:"<>|()!@#$%^&`~]', "", filename)
+
+    # Check if the filename only contains '.py'
+    if sanitized_filename.lower() == '.py':
+        # Append a unique identifier
+
+        #🔴CHANGE TO FILE HASH INSTEAD
+        sanitized_filename = f"_{uuid.uuid4().hex}{sanitized_filename}"
+
+    # Replace spaces with underscores
+    sanitized_filename = shlex.quote(sanitized_filename.replace(" ", "_"))
+
+
+    # Construct the new file path with the sanitized name
+    new_file_path = os.path.join(directory, sanitized_filename)
+
+    # Rename the file
+    shutil.move(file_path, new_file_path)
+
+    return new_file_path
+
+
+def sanitize_and_rename_files(file_paths):
+    sanitized_paths = []
+    for file_path in file_paths:
+        sanitized_path = sanitize_and_rename_file(file_path)
+        sanitized_paths.append(sanitized_path)
+    return sanitized_paths
 
 
 
@@ -153,125 +206,108 @@ try:
     os.system('cls' if os.name == 'nt' else 'clear')
     print_colored_text(logo, 34)
     print_colored_text("\u26A0 Opening file dialog...", 33)
-    print("Select your Python (.py) script of choice.\n")
-    sleep(1)
+    print("Select your Python (.py) script(s) of choice.\n")
+    sleep(.3)
+
+    # Open file selection dialog for multiple files
+    filez = askopenfilenames()
 
 
-    # Open file selection dialog
-    file_selection = askopenfilename()
-    
-    
-
-
-    # Check if a file was selected
-    if not file_selection:
+    # Check if any files were selected
+    if not filez:
         os.system('cls' if os.name == 'nt' else 'clear')
         print_colored_text(logo, 34)
-        print_colored_text("\u26A0 No file selected. Exiting...", 33)
+        print_colored_text("\u26A0 No files selected. Exiting...", 33)
         sleep(2)
         os.system('cls' if os.name == 'nt' else 'clear')
         sys.exit(1)
 
-
-
-    # Check if the selected file has a .py extension
-    while not file_selection.endswith('.py'):
-        print_colored_text("\u2717 {0} is not a .py file. Try again...".format(file_selection), 31)  
-        file_selection = askopenfilename()
-        
-
+    # Check if the selected files have .py extensions
+    for file_selection in filez:
+        while not file_selection.endswith('.py'):
+            print_colored_text("\u2717 {0} is not a .py file. Try again...".format(file_selection), 31)
+            filez = askopenfilenames()
+            file_selection = filez[0]
 
     os.system('cls' if os.name == 'nt' else 'clear')
     print_colored_text(logo, 34)
-    print_colored_text("\u2713 File selected: {0}\n\n".format(file_selection), 32)
-    
-    output_file = input("Enter the name you would like for the output file: ")
-    
-    while output_file == "":
-        os.system('cls' if os.name == 'nt' else 'clear')
+    print_colored_text("\u2713 Files selected:\n{0}\n\n".format('\n'.join(filez).replace(" ", "\\ ")), 32)
 
-        print_colored_text(logo, 34)
-        print_colored_text("\u2713 File selected: {0}\n\n".format(file_selection), 32)
-
-        print_colored_text("\u2717 You entered: {0}\n\n".format("No name detected. Please try again..."), 31)
-        output_file = input("Enter the name you would like for the output file: ")
-
-    
-    print_colored_text("\u26A0 You entered: {0}\n\n".format(output_file), 33)
-
-    if '\\/*?:"<>|' in output_file:
-        print_colored_text("\n \u26A0 Invalid characters detected. Sanatizing file name...", 33)
-    output_file = sanitize_filename(output_file)
-        
-
-    # Calling cython3 to convert to python script to Cython
-
-
-    ### CURRENTLY BROKEN
-    #Builds a shell script containing the commands used to compile the python script
-    sh_builder='''#!/bin/sh
-    
-    cython3 {0} --embed
-
-    # Set PYTHONLIBVER dynamically
-    PYTHONLIBVER=python$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')$(python3-config --abiflags)
-
-    # Execute the command
-    gcc -Os $(python3-config --includes) {1} -o {2} $(python3-config --ldflags) -l$PYTHONLIBVER
-    '''.format(shlex.quote(file_selection), shlex.quote(file_selection.replace('.py', '.c')), shlex.quote(output_file))
-
-    # Open a shell file in write mode
-    sh_file_path = os.path.join(os.path.dirname(__file__), 'gcc_shell.sh')  # Replace with the desired file path
-
-    with open(sh_file_path, 'w') as file:
-        print_colored_text("\u26A0 Executing compliation script...\n\n", 33)
-        # Write content to the file
-        file.write(sh_builder)
-        subprocess.call(['chmod', '+x', sh_file_path])
-
-
-    # Start loading thread
+    # Start animation thread
     t.start()
-    
+
+    # Sanitize and rename selected files and output files
+    sanitized_file_paths = sanitize_and_rename_files(filez)
 
 
+    output_files = []
 
-    # Execute the shell script then remove it and the Cython file
-    try:
-        result = subprocess.run([sh_file_path], check=True, capture_output=True, text=True)
-        os.remove('gcc_shell.sh')
-        os.remove(file_selection.replace('.py', '.c'))
-        os.system('cls' if os.name == 'nt' else 'clear')
+    # Iterate over sanitized files and generate unique output files and shell script paths
+    for selected_file in sanitized_file_paths:
+        output_file = os.path.splitext(os.path.basename(selected_file))[0]
 
-        if result.returncode != 0:
-            print_colored_text("Error Occured: {0}".format(result.stderr), 31)
+        if '\\/*?:"<>|()!@#$%^&`~ ' in output_file:
+            print_colored_text("\n \u26A0 Invalid characters detected. Sanitizing file name...", 33)
+        output_file = sanitize_filename(output_file)
 
-        # Write the result of the terminal to a log file
-        with open(log_file_path, 'w') as log_file:
-            log_file.write(f"Return Code: {result.returncode}\n")
-            log_file.write("\n=== Standard Output ===\n")
-            log_file.write(result.stdout)
-            log_file.write("\n=== Standard Error ===\n")
-            log_file.write(result.stderr)
+        output_files.append(output_file)
 
-        print_colored_text(logo, 34)
-        print_colored_text("\u2713 Python compilation succeeded!", 32)
-        print_colored_text("\u2713 Output File: {0}".format(os.path.join(os.path.dirname(__file__), output_file)), 32)
-        done = True
-        subprocess.call(["xdg-open", "{0}".format(os.path.join(os.path.dirname(__file__)))])
+    # Iterate over sanitized files and their corresponding output_files
+    for selected_file, output_file in zip(sanitized_file_paths, output_files):
+        # Calling cython3 to convert to python script to Cython
+        # Build a unique shell script containing the commands used to compile each Python script
+        # Generate the Cython C file explicitly
+        
+        escaped_output_file = output_file.replace(" ", "\\ ")
+        escaped_selected_file = selected_file.replace(" ", "\\ ")
 
-    except subprocess.CalledProcessError as e:
-        os.remove('gcc_shell.sh')
-        os.remove(file_selection.replace('.py', '.c'))
-        print_colored_text(f"Command failed with return code {e.returncode}", 31)
-        with open(log_file_path, 'w') as log_file:
-            log_file.write(f"Return Code: {e.returncode}\n")
-            log_file.write("\n=== Standard Output ===\n")
-            log_file.write(e.stdout)
-            log_file.write("\n=== Standard Error ===\n")
-            log_file.write(e.stderr)
-        done = True
-        sys.exit(1)
+        cython_command = f'cython3 "{selected_file}" --embed'
+
+        # Set PYTHONLIBVER dynamically
+        PYTHONLIBVER = f'''python$(python3 -c "import sys; print('.'.join(map(str, sys.version_info[:2])))")$(python3-config --abiflags)'''
+
+        # Corrected gcc command
+        gcc_command = f'gcc -Os $(python3-config --includes) "{selected_file}" -o "{output_file}" $(python3-config --ldflags) -l{PYTHONLIBVER}'
+
+        # Combine the commands into a shell script
+        sh_builder = f'''#!/bin/sh
+
+        {cython_command.replace(selected_file, escaped_selected_file)}
+        {gcc_command.replace(output_file, escaped_output_file).replace('.py', '.c')}
+        '''
+
+
+        # Open a shell file in write mode
+        sh_file_path = os.path.join(os.path.dirname(__file__), f'gcc_shell_{output_file}.sh')
+     
+        with open(sh_file_path, 'w') as file:
+            print_colored_text(f"\n\u26A0 Executing compilation script for {selected_file}...\n\n", 33)
+            # Write content to the file, wrapping file paths in double-quotes
+            file.write(sh_builder)
+            subprocess.call(['chmod', '+x', sh_file_path])
+
+        # Execute the shell script then remove it and the Cython file
+        try:
+            result = subprocess.run([sh_file_path], check=True, capture_output=True, text=True)
+            os.remove(sh_file_path)  # Remove the unique shell script
+            os.remove(selected_file.replace('.py', '.c'))
+            # os.system('cls' if os.name == 'nt' else 'clear')
+            done = True
+
+            # ...
+
+        except subprocess.CalledProcessError as e:
+            # os.remove(sh_file_path)  # Remove the unique shell script
+            # os.remove(selected_file.replace('.py', '.c'))
+            print_colored_text(f"Command failed with return code {e.returncode}", 31)
+            with open(log_file_path, 'w') as log_file:
+                log_file.write(f"Return Code: {e.returncode}\n")
+                log_file.write("\n=== Standard Output ===\n")
+                log_file.write(e.stdout)
+                log_file.write("\n=== Standard Error ===\n")
+                log_file.write(e.stderr)
+            done = True
+            sys.exit(1)
 
 # except KeyboardInterrupt:
 except KeyboardInterrupt:
